e490dabddd5dede2d3cc90bf91f3317342c27702
Michael Humphrey <mhumphree@gmail.com>
2017-01-17 18:07:54 -0800

    Created initial code solution

---
+++ code/Hephaestus/Hephaestus/main.c
@@ -0,0 +1,18 @@
+/*
+ * Hephaestus.c
+ *
+ * Created: 1/17/2017 6:05:07 PM
+ * Author : Michael Humphrey
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
