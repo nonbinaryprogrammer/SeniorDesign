e99a6b69c444db44122395ef397ea2ae5f92f6fd
Michael Humphrey <mhumphree@gmail.com>
2017-02-15 22:32:44 -0800

    Finished Idle phase with flag check

--- code/Hephaestus/Hephaestus/idle.c
+++ code/Hephaestus/Hephaestus/idle.c
@@ -3,16 +3,18 @@
  *
  * Created: 1/20/2017 3:36:32 PM
  *  Author: Michael Humphrey
- */ 
+ */
+
+#include "RSXAVRD.h"
+
+// Grab variable from RSXAVRD.h
+extern uint8_t timer_event0_flg;
 
  // First phase of payload deployment. Wait for TE-R lines to go high, then return.
  int idle(void) {
 	
-	while (1) {
-		// TODO: Test if either TE-R lines are high
-		// Port D, pins 2 & 3
-		if (1) {
-			return 0;
-		}
-	}
- }
+	// Infinite loop until start experiment flag is set to 1
+	while (timer_event0_flg == 0) {}
+
+	return 0;
+}
