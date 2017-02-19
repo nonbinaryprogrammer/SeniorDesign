e60b6126224a84fc952af8838f99edfe68695020
U-n2anewt\n2a newt <n2a newt@n2anewt.(none)>
2017-02-19 00:03:15 -0800

    Updated Drivers 02-18-16

--- code/Hephaestus/Hephaestus/RSXAVRD.c
+++ code/Hephaestus/Hephaestus/RSXAVRD.c
@@ -1,7 +1,7 @@
 /* Designer: Jonathan Hardman
  * Filename: RSXAVRD.c
- * Version: 1.1
- * Date: 01/31/17
+ * Version: 2.0
+ * Date: 02/18/17
  * Description: AVR ATmega64 I/O Drivers for RS-X Project
 */
 
@@ -11,7 +11,7 @@
 #include <util/delay.h>
 #include "RSXAVRD.h"
 
-/*********Parallel Lines Config*********/
+/********I/O Config********/
 #define LINES_DDR &DDRD
 #define LINES_PORT &PORTD
 #define LINE0_PIN 4
@@ -19,7 +19,6 @@
 #define LINE2_PIN 6
 #define LINE3_PIN 7
 
-/*********Motor Driver Config*********/
 #define MOTOR_EN_DDR &DDRF
 #define MOTOR_EN_PORT &PORTF
 #define MOT0_EN_PIN 2
@@ -52,87 +51,109 @@
 #define MOT2_CALIB_INT 2
 #define MOT3_CALIB_INT 3
 #define MOT4_CALIB_INT 4
-#define RSX_TE0_INT 5
-#define RSX_TE1_INT 6
 
-volatile uint8_t calibration = 0x00;
-volatile uint8_t timer_event0_flg = 0x00;
-volatile uint8_t timer_event1_flg = 0x00;
+#define RSX_TE0_INT 6
+#define RSX_TE1_INT 7
 
-volatile uint16_t time = 0x00; //in tenths of a second
+#define CAMERA_EN_DDR &DDRA
+#define CAMERA_EN_PORT &PORTA
+#define CAMERA_EN_PIN 6
+
+
+/*********Variables*********/
+uint8_t calibration_flg = 0x00; //Will hold number other than 0 when calibration switch triggered
+uint8_t timer_event0_flg = 0x00; //Will switch to 1 once experiment begin signal received
+uint8_t timer_event1_flg = 0x00; //Will switch to 1 once emergency retract signel receieved
+uint16_t time = 0x0000; //Holds time count in tenths of a second from when timer_counter_enable() is turned on
+
+uint16_t get_time(){return time;}
+uint8_t get_TE0_status(){return timer_event0_flg;}
+uint8_t get_TE1_status(){return timer_event1_flg;}
+uint8_t get_calibration_status(){return calibration_flg;}
 
 /*********Interrupts*********/
 //Calibration Interrupt function for motor 0
 ISR(INT0_vect){
-	calibration |= 0x01;
+	calibration_flg |= 0x01;
 }
 
 //Calibration Interrupt function for motor 1
 ISR(INT1_vect){
-	calibration |= 0x02;
+	calibration_flg |= 0x02;
 }
 
 //Calibration Interrupt function for motor 2
 ISR(INT2_vect){
-	calibration |= 0x04;
+	calibration_flg |= 0x04;
 }
 
 //Calibration Interrupt function for motor 3
 ISR(INT3_vect){
-	calibration |= 0x08;
+	calibration_flg |= 0x08;
 }
 
 //Calibration Interrupt function for motor 4
 ISR(INT4_vect){
-	calibration |= 0x10;
+	calibration_flg |= 0x10;
 }
 
 //Timer Event 0 Interrupt Function - Begin Experiment
-ISR(INT5_vect){
+ISR(INT6_vect){
 	timer_event0_flg = 0x01;
 }
 
 //Timer Event 1 Interrupt Function - Emergency Shutoff
-ISR(INT6_vect){
+ISR(INT7_vect){
 	timer_event1_flg = 0x01;
-	/* Or do something else here */
+	/* CONSIDER REVISING THIS FOR FINAL */
 }
 
-ISR(TIMER0_OVF_vect){
-	PORTD ^= 0x01;
-}
-
+//Timer Counter Interrupt - increments time variable every 0.1 sec
 ISR(TIMER1_COMPA_vect){
-	//PORTD ^= 0x01; //For timing testing
+	PORTD ^= 0x01; //For timing testing
 	time++;
 }
 
 /***********************************General***********************************/
 void AVR_init(){
+	//Motor Data Direction
 	*MOTOR_EN_DDR |= (1<<MOT0_EN_PIN | 1<<MOT1_EN_PIN | 1<<MOT2_EN_PIN | 1<<MOT3_EN_PIN | 1<<MOT4_EN_PIN | 1<<MOT5_EN_PIN);
 	*MOTOR_DIR_DDR |= (1<<MOT0_DIR_PIN | 1<<MOT1_DIR_PIN | 1<<MOT2_DIR_PIN | 1<<MOT3_DIR_PIN | 1<<MOT4_DIR_PIN | 1<<MOT5_DIR_PIN);
 	*MOTOR_STEP_DDR |= (1<<MOT0_STEP_PIN | 1<<MOT1_STEP_PIN | 1<<MOT2_STEP_PIN | 1<<MOT3_STEP_PIN | 1<<MOT4_STEP_PIN | 1<<MOT5_STEP_PIN);
+	
+	//Parallel lines pin direction
+	*LINES_DDR |= (1<<LINE0_PIN | 1<<LINE1_PIN | 1<<LINE2_PIN | 1<<LINE3_PIN);
+	
+	//Camera Data Direction
+	*CAMERA_EN_DDR |= (1<<CAMERA_EN_PIN);
+	
+	//External Interrupt Settings
 	EICRA |= (1<<ISC31 | 1<<ISC30 | 1<<ISC21 | 1<<ISC20 | 1<<ISC11 | 1<<ISC10 | 1<<ISC01 | 1<<ISC00);
 	EICRB |= (1<<ISC71 | 1<<ISC70 | 1<<ISC61 | 1<<ISC60 | 1<<ISC51 | 1<<ISC50 | 1<<ISC41 | 1<<ISC40);
 	
-	//8-bit Fast-PWM mode, no prescaling, 255 top, OCR3A comp (set on match) 
-	TCCR1B |= (1<<WGM12 | 1<<CS11 | 1<<CS10); //64 prescale
+	//TimerCounter1: 16-bit CTC mode, 64 prescale, 12500 top, no output latching
+	TCCR1B |= (1<<WGM12 | 1<<CS11 | 1<<CS10);
 	OCR1A = 0x30D4;
-	DDRD |= 0x01;
+	
+	//Enable ADC, single conversion mode, use external common GND, right-adjusted.
+	ADMUX = (1<<REFS0);
+	ADCSRA = (1<<ADEN);
 }
 
 void timer_counter_enable(uint8_t flag){
 	cli();
 	if(flag == 0x01)
 		TIMSK |= (1<<OCIE1A);
-	else
-		TIMSK |= (1<<OCIE1A);
+	else{
+		TIMSK &= ~(1<<OCIE1A);
+		//time = 0x0000;
+	}
 	sei();
 }
 
 void timer_event_enable(uint8_t event, uint8_t flag){
 	cli();
-	uint8_t eventID [2] = {RSX_TE0_INT, RSX_TE1_INT};
+	uint8_t eventID[2] = {RSX_TE0_INT, RSX_TE1_INT};
 	if(flag == 0x01)
 		EIMSK |= (1<<eventID[event]);
 	else{
@@ -143,6 +164,29 @@ void timer_event_enable(uint8_t event, uint8_t flag){
 	sei();
 }
 
+void send_code(uint8_t code, uint8_t ms){
+	uint8_t i;
+	while(!((PINE & 0x08) > 0x00)){} //wait for PRS
+	_delay_us(5); //wait for end of READ phase
+	*LINES_PORT |= ((code&0x01)<<LINE0_PIN | (code&0x02)<<LINE1_PIN | (code&0x04)<<LINE2_PIN | (code&0x08)<<LINE3_PIN); //send code
+	for(i=0;i<ms;i++)
+		_delay_ms(1);
+	while(!((PINE & 0x08) > 0x00)){} //wait for pulse
+	_delay_us(5); //wait for end of READ phase
+	*LINES_PORT &= ~(1<<LINE0_PIN | 1<<LINE1_PIN | 1<<LINE2_PIN | 1<<LINE3_PIN); //reset to 0's
+}
+
+/**************************************Device Functions**************************************/
+void camera_enable(uint8_t flag){
+	static uint8_t power = 0x00;
+	if (((flag == 0x01)&(power == 0x00))||((flag == 0x00)&(power == 0x01))){
+		*CAMERA_EN_PORT |= (1<<CAMERA_EN_PIN);
+		_delay_ms(100);
+		*CAMERA_EN_PORT &= ~(1<<CAMERA_EN_PIN);
+		power = (power + 1) % 2;
+	}
+}
+
 uint8_t touch_sensor_check(){
 	if((PINE & 0x04) > 0x00)
 		return 0x01;
@@ -150,16 +194,28 @@ uint8_t touch_sensor_check(){
 		return 0x00;
 }
 
-void send_code(uint8_t code, uint8_t ms){
-	uint8_t i;
-	*LINES_DDR |= (1<<LINE0_PIN | 1<<LINE1_PIN | 1<<LINE2_PIN | 1<<LINE3_PIN);
-	*LINES_PORT |= ((code&0x01)<<LINE0_PIN | (code&0x02)<<LINE1_PIN | (code&0x04)<<LINE2_PIN | (code&0x08)<<LINE3_PIN);
-	for(i=0;i<ms;i++)
-		_delay_ms(1);
-	*LINES_PORT &= ~(1<<LINE0_PIN | 1<<LINE1_PIN | 1<<LINE2_PIN | 1<<LINE3_PIN);
+uint16_t temperature_probe_sample(uint8_t probe){
+	if(probe == 0x00)
+		ADMUX &= ~(1<<MUX0);
+	else
+		ADMUX |= (1<<MUX0);
+	ADCSRA |= (1<<ADSC); //Starts conversion
+	while(ADCSRA & (1<<ADSC));
+	return ADC;
 }
 
 /***********************************Motor Driver Functions***********************************/
+void motor_calibration_enable(uint8_t motor, uint8_t flag){
+	uint8_t motorCBID[6] = {MOT0_CALIB_INT, MOT1_CALIB_INT, MOT2_CALIB_INT, MOT3_CALIB_INT, MOT4_CALIB_INT};
+	cli();
+	if(flag == 0x01)
+		EIMSK |= (1<<motorCBID[motor]);
+	else
+		EIMSK &= (1<<motorCBID[motor]);
+	calibration_flg = 0x00;
+	sei();
+}
+
 void motor_pwr(uint8_t motor, uint8_t flag){
 	uint8_t motorENID[6] = {MOT0_EN_PIN, MOT1_EN_PIN, MOT2_EN_PIN,
 					   MOT3_EN_PIN, MOT4_EN_PIN, MOT5_EN_PIN};
@@ -184,7 +240,7 @@ void motor_step(uint8_t motor, uint16_t steps, uint16_t speed){
 	speed = -99*(speed)+10000;
 	steps = 2*steps;
 	for (j=0;j<steps;j++){
-		if(calibration == 0x00){
+		if(calibration_flg == 0x00){
 			*MOTOR_STEP_PORT ^= (1<<motorSTEPID[motor]);
 			for (k=0;k<speed;k++)
 				_delay_us(1);
@@ -194,14 +250,3 @@ void motor_step(uint8_t motor, uint16_t steps, uint16_t speed){
 		}
 	}
 }
-
-void calibration_enable(uint8_t motor, uint8_t flag){
-	uint8_t motorCBID[6] = {MOT0_CALIB_INT, MOT1_CALIB_INT, MOT2_CALIB_INT, MOT3_CALIB_INT, MOT4_CALIB_INT};
-	cli();
-	if(flag == 0x01)
-		EIMSK |= (1<<motorCBID[motor]);
-	else
-		EIMSK &= (1<<motorCBID[motor]);	
-	sei();
-}
-
--- code/Hephaestus/Hephaestus/RSXAVRD.h
+++ code/Hephaestus/Hephaestus/RSXAVRD.h
@@ -1,38 +1,68 @@
 /* Designer: Jonathan Hardman
  * Filename: RSXAVRD.h
- * Version: 1.1
- * Date: 01/31/17
+ * Version: 2.0
+ * Date: 02/18/17
  * Description: AVR ATmega64 I/O Drivers for RS-X Project
 */
+/******************Variables/Flags******************/
+//Returns time count in 0.1 seconds,
+//Can be 0-65535
+uint16_t get_time(void);
+
+//Returns Timer Event 0 signal state,
+//0 for not received or 1 for received
+uint8_t get_TE0_status(void);
+
+//Returns Timer Event 1 signal state,
+//0 for not received or 1 for received
+uint8_t get_TE1_status(void);
+
+//Returns Calibration Status, 
+//0 for no calibration, 1=MOT0, 2=MOT1, 4=MOT2, 8=MOT3, 16=MOT4 calibrated
+uint8_t get_calibration_status(void);
 
 /**********************General**********************/
 //Sets up microcontroller I/O properties, run before everything.
 void AVR_init(void);
 
 //Enable or disable the time keeping of the AVR
+//flag: 1=On, 0=Off, time reset on turn-off
 void timer_counter_enable(uint8_t flag);
 
-//Enable or Disable Timer Event Interrupt flags, event: 0 or 1, flag: 1=On, 0=Off
+//Enable or Disable Timer Event Interrupt flags, 
+//event: 0 or 1, flag: 1=On, 0=Off
 void timer_event_enable(uint8_t event, uint8_t flag);
-
-//Checks for any successful touch on all touch point switches, reutrns 1 for connection, 0 for no connection
-uint8_t touch_sensor_check(void);
 
 //Sends lower 4bits provided for ms amount of milliseconds
 void send_code(uint8_t code, uint8_t ms);
 
-/*******************Motor Control*******************/
-//Turn on/off for motor selected, motor: 0-4, flag: 1=On 0=Off 
-void calibration_enable(uint8_t motor, uint8_t flag);
+/**********************Devices**********************/
+//Turn on/off the external camera,
+//flag: 0=Off, 1=On
+void camera_enable(uint8_t flag);
 
-//Turns on/off motor selected, motor: 0-5, flag: 1=On 0=Off
+//Checks for any successful touch on all touch point switches, 
+//returns 1 for connection, 0 for no connection
+uint8_t touch_sensor_check(void);
+
+//Samples and returns probe temperature in 10bit format, 
+//probe: 0=Internal, 1=External, returns 16bit val with lower 10bits from ADC
+uint16_t temperature_probe_sample(uint8_t probe);
+
+/*******************Motor Control*******************/
+//Turn on/off for motor selected, 
+//motor: 0-4, flag: 1=On 0=Off 
+void motor_calibration_enable(uint8_t motor, uint8_t flag);
+
+//Turns on/off motor selected, 
+//motor: 0-5, flag: 1=On 0=Off
 void motor_pwr(uint8_t motor, uint8_t flag);
 
-//Sets the motor turning direction, motor: 0-5, dir: 1=CCW 0=CW
+//Sets the motor turning direction, 
+//motor: 0-5, dir: 1=CCW 0=CW
 void motor_dir(uint8_t motor, uint8_t dir);
 
-//Steps the motor at a given speed, motor: 0-5, steps: <65535, speed: 0-100
+//Steps the motor at a given speed, 
+//motor: 0-5, steps: <65535, speed: 0-100
 void motor_step(uint8_t motor, uint16_t steps, uint16_t speed);
-
-
 
