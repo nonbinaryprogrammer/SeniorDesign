881781732d0ca8f33ec3c9b191e88054f9f881b7
Michael Humphrey <mhumphree@gmail.com>
2017-03-08 11:50:21 -0800

    Updated telemtry header files. I don't know what I'm doing

--- code/Hephaestus/Hephaestus/telemetry.c
+++ code/Hephaestus/Hephaestus/telemetry.c
@@ -4,3 +4,28 @@
  * Created: 1/18/2017 10:44:49 AM
  *  Author: Michael Humphrey
  */ 
+
+// Sends a code with the predefined TELEMETRY_TIME constant.
+void inline send_code(uint8_t code) {
+	send_code(code, TELEMTRY_TIME);
+}
+
+
+// Log a message to the SD card
+void SD_log(char* message) {
+	// Write elapsed time to SD card
+	char *result = (char*) malloc(6*sizeof(char)); // Max value of get_time() is 65535, which is 5 characters, plus 1 for null terminator
+
+	if (result == NULL)
+		return;
+
+	itoa(get_time(), result, 10);
+	// Write result string to SD card
+	free(result);
+
+	// Write separator to SD card
+
+	// Write message to SD card
+
+	// Write line terminator to SD card
+}
