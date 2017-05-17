2fd734bd3fb69b046577058bc5888c232e1f98a7
Helena Bales <balesh@oregonstate.edu>
2017-05-16 20:53:27 -0700

    find the next point in the path by checking all 80 neighbors

--- code/Hephaestus/Hephaestus/science.c
+++ code/Hephaestus/Hephaestus/science.c
@@ -17,7 +17,11 @@ int science(){
 	int M1_DIF, M2_DIF, M3_DIF, M4_DIF;
 	int M1_STP, M2_STP, M3_STP, M4_STP;
 	int scale_factor = 0.225;
+	char path_step;
 
+	//TODO reference CSpace in program memory (the address of a 4D array in program memory)
+	char**** cspace;
+		
 	// init motor values to 0
 	M1_POS = 0;
 	M2_POS = 0;
@@ -33,87 +37,416 @@ int science(){
 	motor_pwr(3, 1);
 	motor_pwr(4, 1);
 
-	//set motor direction to be forward
-	motor_dir(1, 0);
-	motor_dir(2, 0);
-	motor_dir(3, 0);
-	motor_dir(4, 0);
-	
-	// TODO retrieve the next position
-	M1_NXT = -1;	// Dummy data
-	M2_NXT = -1;
-	M3_NXT = -1;
-	M4_NXT = -1;
-	
-	//change _NXT values to be less than 360
-	if(M1_NXT > 359) {
-		M1_NXT = M1_NXT - 359;
-	}
+	//repeat for length of path
+	while(1) {
+		//set motor direction to be forward
+		motor_dir(1, 0);
+		motor_dir(2, 0);
+		motor_dir(3, 0);
+		motor_dir(4, 0);
+		
+		//find the next point in the path
+		path_step = cspace[M1_POS][M2_POS][M3_POS][M4_POS];
 
-	if(M2_NXT > 359) {
-		M2_NXT = M2_NXT - 359;
-	}
+		//change 1
+		//MN+/-1
+		if(M1_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+		} else if(M1_POS > 0 && cspace[M1_POS-1][M2_POS][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+		} else if(M2_POS < 36 && cspace[M1_POS][M2_POS+1][M3_POS][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+		} else if(M2_POS > 0 && cspace[M1_POS][M2_POS-1][M3_POS][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+		} else if(M3_POS < 36 && cspace[M1_POS][M2_POS][M3_POS+1][M4_POS] == path_step + 1) {
+			M3_NXT = M3_POS + 1;
+		} else if(M3_POS > 0 && cspace[M1_POS][M2_POS][M3_POS-1][M4_POS] == path_step + 1) {
+			M3_NXT = M3_POS - 1;
+		} else if(M4_POS < 36 && cspace[M1_POS][M2_POS][M3_POS][M4_POS+1] == path_step + 1) {
+			M4_NXT = M4_POS + 1;
+		} else if(M4_POS > 0 && cspace[M1_POS][M2_POS][M3_POS][M4_POS-1] == path_step + 1) {
+			M4_NXT = M4_POS - 1;
 
-	if(M3_NXT > 359) {
-		M3_NXT = M3_NXT - 359;
-	}
+		//change 2
+		//M1+1, MN+/-1
+		} else if(M1_POS < 36 && M2_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+		} else if(M1_POS < 36 && M3_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS < 36 && M3_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M1_POS < 36 && M4_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M4_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M1-1, MN+/-1
+		} else if(M1_POS > 0 && M2_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+		} else if(M1_POS > 0 && M3_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS > 0 && M3_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M1_POS > 0 && M4_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M4_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M2+1, MN+/-1
+		} else if(M2_POS < 36 && M3_POS < 36 && cspace[M1_POS][M2_POS+1][M3_POS+1][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M2_POS < 36 && M3_POS > 0 && cspace[M1_POS][M2_POS+1][M3_POS-1][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M2_POS < 36 && M4_POS < 36 && cspace[M1_POS][M2_POS+1][M3_POS][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS < 36 && M4_POS > 0 && cspace[M1_POS][M2_POS+1][M3_POS][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M2-1, MN+/-1
+		} else if(M2_POS > 0 && M3_POS < 36 && cspace[M1_POS][M2_POS-1][M3_POS+1][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M2_POS > 0 && M3_POS > 0 && cspace[M1_POS][M2_POS-1][M3_POS-1][M4_POS] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M2_POS > 0 && M4_POS < 36 && cspace[M1_POS][M2_POS-1][M3_POS][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS > 0 && M4_POS > 0 && cspace[M1_POS][M2_POS-1][M3_POS][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M3+1, MN+/-1
+		} else if(M3_POS < 36 && M4_POS < 36 && cspace[M1_POS][M2_POS][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M3_POS < 36 && M4_POS > 0 && cspace[M1_POS][M2_POS][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M3-1, M4+/-1
+		} else if(M3_POS > 0 && M4_POS < 36 && cspace[M1_POS][M2_POS][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M3_POS > 0 && M4_POS > 0 && cspace[M1_POS][M2_POS][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
 
-	if(M4_NXT > 359) {
-		M4_NXT = M4_NXT - 359;
-	}
-	
-	// calculate change in each motor (in degrees)
-	M1_DIF = M1_NXT - M1_POS;
-	M2_DIF = M2_NXT - M2_POS;
-	M3_DIF = M3_NXT - M3_POS;
-	M4_DIF = M4_NXT - M4_POS;
+		//change 3
+		//M1+1, M2+/-1, M3+/-1
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS > 0 && cspace[M1_POS+1][M2_POS+1][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS < 36 && cspace[M1_POS+1][M2_POS-1][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+		//M1+1, M2+/-1, M4+/-1
+		} else if(M1_POS < 36 && M2_POS < 36 && M4_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS < 36 && M4_POS > 0 && cspace[M1_POS+1][M2_POS+1][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M4_POS < 36 && cspace[M1_POS+1][M2_POS-1][M4_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M4_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M1+1, M3+/-1, M4+/-1
+		} else if(M1_POS < 36 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS < 36 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS+1][M2_POS][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS+1][M2_POS][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M1-1, M2+/-1, M3+/-1
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS < 36 && cspace[M1_POS-1][M2_POS+1][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS > 0 && cspace[M1_POS-1][M2_POS+1][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS < 36 && cspace[M1_POS-1][M2_POS-1][M3_POS+1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS > 0 && cspace[M1_POS-1][M2_POS-1][M3_POS-1][M4_POS] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+		//M1-1, M2+/-1, M4+/-1
+		} else if(M1_POS > 0 && M2_POS < 36 && M4_POS < 36 && cspace[M1_POS-1][M2_POS+1][M3_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS < 36 && M4_POS > 0 && cspace[M1_POS-1][M2_POS+1][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M4_POS < 36 && cspace[M1_POS-1][M2_POS-1][M4_POS][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M4_POS > 0 && cspace[M1_POS-1][M2_POS-1][M3_POS][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M4_NXT = M4_POS - 1;
+		//M1-1, M3+/-1, M4+/-1
+		} else if(M1_POS > 0 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS-1][M2_POS][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS-1][M2_POS][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS > 0 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS-1][M2_POS][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS-1][M2_POS][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M2+1, M3+/-1, M4+/-1 
+		} else if(M2_POS < 36 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS][M2_POS+1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS < 36 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS][M2_POS+1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M2_POS < 36 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS][M2_POS+1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS < 36 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS][M2_POS+1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M2-1, M3+/-1, M4+/-1 
+		} else if(M2_POS > 0 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS][M2_POS-1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS > 0 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS][M2_POS-1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M2_POS > 0 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS][M2_POS-1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M2_POS > 0 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS][M2_POS-1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
 
-	// reverse motor direction for negative difference
-	if(M1_DIF < 0) {
-		motor_dir(1, 1);
-		M1_DIF = M1_DIF * -1;
-	}
+		//change 4
+		//(M1-2)+1, (M3-4)+/-1
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS+1][M2_POS+1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS+1][M2_POS+1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS < 36 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS+1][M2_POS+1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M1+1, M2-1, (M3-4)+/-1
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS+1][M2_POS-1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS+1][M2_POS-1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS < 36 && M2_POS > 0 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS+1][M2_POS-1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS + 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M1-1, M2+1, (M3-4)+/-1
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS-1][M2_POS+1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS-1][M2_POS+1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS-1][M2_POS+1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS < 36 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS-1][M2_POS+1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS + 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		//M1+1, M2-1, (M3-4)+/-1
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS < 36 && M4_POS < 36 && cspace[M1_POS-1][M2_POS-1][M3_POS+1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS < 36 && M4_POS > 0 && cspace[M1_POS-1][M2_POS-1][M3_POS+1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS + 1;
+			M4_NXT = M4_POS - 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS > 0 && M4_POS < 36 && cspace[M1_POS-1][M2_POS-1][M3_POS-1][M4_POS+1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS + 1;
+		} else if(M1_POS > 0 && M2_POS > 0 && M3_POS > 0 && M4_POS > 0 && cspace[M1_POS-1][M2_POS-1][M3_POS-1][M4_POS-1] == path_step + 1) {
+			M1_NXT = M1_POS - 1;
+			M2_NXT = M2_POS - 1;
+			M3_NXT = M3_POS - 1;
+			M4_NXT = M4_POS - 1;
+		} else {
+			//end of path reached
+			//TODO send telemetry code
+			return 0;
+		}
 
-	if(M2_DIF < 0) {
-		motor_dir(2, 1);
-		M2_DIF = M2_DIF * -1;
-	}
+		//change _NXT values to be less than 360
+		if(M1_NXT > 359) {
+			M1_NXT = M1_NXT - 359;
+		}
 
-	if(M3_DIF < 0) {
-		motor_dir(3, 1);
-		M3_DIF = M3_DIF * -1;
-	}
+		if(M2_NXT > 359) {
+			M2_NXT = M2_NXT - 359;
+		}
 
-	if(M4_DIF < 0) {
-		motor_dir(4, 1);
-		M4_DIF = M4_DIF * -1;
-	}
+		if(M3_NXT > 359) {
+			M3_NXT = M3_NXT - 359;
+		}
 
-	// convert degress to steps
-	M1_STP = M1_DIF / scale_factor;
-	M2_STP = M2_DIF / scale_factor;
-	M3_STP = M3_DIF / scale_factor;
-	M4_STP = M4_DIF / scale_factor;
+		if(M4_NXT > 359) {
+			M4_NXT = M4_NXT - 359;
+		}
+		
+		// calculate change in each motor (in degrees)
+		M1_DIF = M1_NXT - M1_POS;
+		M2_DIF = M2_NXT - M2_POS;
+		M3_DIF = M3_NXT - M3_POS;
+		M4_DIF = M4_NXT - M4_POS;
 
-	// move each motor that many steps
-	motor_step(1, M1_STP, 28, 99);
-	motor_step(2, M2_STP, 28, 99);
-	motor_step(3, M3_STP, 28, 99);
-	motor_step(4, M4_STP, 28, 99);
+		// reverse motor direction for negative difference
+		if(M1_DIF < 0) {
+			motor_dir(1, 1);
+			M1_DIF = M1_DIF * -1;
+		}
 
-	// set the current position
-	M1_POS = M1_NXT;
-	M2_POS = M2_NXT;
-	M3_POS = M3_NXT;
-	M4_POS = M4_NXT;
+		if(M2_DIF < 0) {
+			motor_dir(2, 1);
+			M2_DIF = M2_DIF * -1;
+		}
 
-	if(touch_sensor_check() == 0x01){
-		telemetry_send_code(TOUCH_SENSOR_1_ENGAGED); // For now, always send TOUCH_SENSOR_1_ENGAGED.
-	}
+		if(M3_DIF < 0) {
+			motor_dir(3, 1);
+			M3_DIF = M3_DIF * -1;
+		}
 
-	// code to return arm to callibrated "home" position
-	if(get_calibration_status() != 0x01){ // or whichever motor refers to the home position
-		status = 1;
+		if(M4_DIF < 0) {
+			motor_dir(4, 1);
+			M4_DIF = M4_DIF * -1;
+		}
+
+		// convert degress to steps
+		M1_STP = M1_DIF / scale_factor;
+		M2_STP = M2_DIF / scale_factor;
+		M3_STP = M3_DIF / scale_factor;
+		M4_STP = M4_DIF / scale_factor;
+
+		// move each motor that many steps
+		motor_step(1, M1_STP, 28, 99);
+		motor_step(2, M2_STP, 28, 99);
+		motor_step(3, M3_STP, 28, 99);
+		motor_step(4, M4_STP, 28, 99);
+
+		// set the current position
+		M1_POS = M1_NXT;
+		M2_POS = M2_NXT;
+		M3_POS = M3_NXT;
+		M4_POS = M4_NXT;
+
+		if(touch_sensor_check() == 0x01){
+			telemetry_send_code(TOUCH_SENSOR_1_ENGAGED); // For now, always send TOUCH_SENSOR_1_ENGAGED.
+		}
+
+		// code to return arm to callibrated "home" position
+		if(get_calibration_status() != 0x01){ // or whichever motor refers to the home position
+			status = 1;
+		}
+
 	}
 
 	return status;
